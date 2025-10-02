7c89617a7ef952fc52893ef2de4e61ff7797471f
Michael Mercer <91555619+MicMe23@users.noreply.github.com>
2025-10-02 11:44:12 -0400

    hash join inner

---
+++ Hash_join.py
@@ -0,0 +1,35 @@
+
+from collections import defaultdict
+from typing import List, Dict, Iterable
+
+def hash_join_inner(left, right, key):
+    # Requirements :
+    #  - left_rows / right_rows: lists of dicts
+    #  - key exists in both sides; None keys don't match
+    #  - duplicate keys produce the cross product of matches for now.
+    #  returns: list of merged dicts
+    if len(left) > len(right):
+        build, probe = (left, right)
+    else:
+        build, probe = (right, left)
+
+    # assign to know which side is which when merging
+    build_is_left = build is left  
+
+     # Build: key -> list of rows
+    H = defaultdict(list)
+    for r in build:
+        H[r[key]].append(r)
+    
+    # Probe
+    out = []
+    for r in probe:
+        k = r[key]
+        if k in H:
+            for b in H[k]:
+                if build_is_left:
+                    joined = {**b, **r}     # left fields then right fields
+                else:
+                    joined = {**r, **b}     # right fields then left fields
+                out.append(joined)
+    return out
